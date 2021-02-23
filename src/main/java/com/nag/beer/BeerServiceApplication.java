3fdcaaf19f721ba85079eef905d4971419b81a85
Desaraju <desaraju02@gmail.com>
2021-02-23 17:53:37 +0530

    Created Controller, DTO and Entity

--- src/main/java/com/nag/beer/BeerServiceApplication.java
+++ src/main/java/com/nag/beer/BeerServiceApplication.java
@@ -2,8 +2,9 @@ package com.nag.beer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class BeerServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/nag/beer/web/controller/BeerController.java
@@ -0,0 +1,40 @@
+package com.nag.beer.web.controller;
+
+
+import com.nag.beer.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-05-12.
+ */
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+}
